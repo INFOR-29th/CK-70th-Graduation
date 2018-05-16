7a955f89d7f86a08b981f2ea3ce35360f98854d3
TaWeiTu <tu.da.wei@gmail.com>
2018-05-16 18:10:20 +0800

    create api.js

--- api.js
+++ api.js
@@ -1,19 +1,26 @@
 require('./db.js')
-const express = require('express');
+const express = require('express')
+const app     = express()
+const config  = require('./config.json')
 const router = express.Router({ mergeParams: true });
+const port    = config.port || 3000;
 
-router.get('/ann', (res, req) => {
-    console.log(`params = ${params}`);
+router.get('/ann/:id', (req, res, next) => {
+    let id = req.params.id;
+    console.log(id);
+    // Ann.fi()
 });
 
-router.post('/ann', (req, res) => {
+router.post('/ann', (req, res, next) => {
 
 });
 
-router.put('/ann', (req, res) => {
+router.put('/ann', (req, res, next) => {
 
 });
 
-router.delete('/ann', (req, res) => {
+router.delete('/ann', (req, res, next) => {
 
 });
+
+module.exports = router;
--- app.js
+++ app.js
@@ -2,28 +2,22 @@ require('./db.js')
 const express = require('express')
 const app     = express()
 const config  = require('./config.json')
+const bodyParser = require('body-parser');
 const port    = config.port || 3000;
 
 app.use('/assets', express.static(__dirname + '/assets'))
 app.use('/images', express.static(__dirname + '/images'))
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/api/ann/:id', (req, res) => {
-    let id = req.params.id;
-    Ann.findById()
-});
+const apiRouter = require('./api.js');
+app.use('/api', apiRouter);
 
-app.post('/api/ann', (req, res) => {
-
-});
-
-app.put('/api/ann', (req, res) => {
-
-});
-
-app.delete('/api/ann', (req, res) => {
-
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html', () => {
+        res.end();
+    });
 });
 
 app.listen(port, () => {
-  console.log(`Running on port ${port}`);
+    console.log(`Running on port ${port}`);
 });
