5c5b661e998b8e1535554876aec9c795a3256e55
michaelwan <michaelwan0906@hotmail.com>
2018-05-16 17:30:11 +0800

    setup index.html

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const config = require('./config.json')
+const port = config.port || 3000;
+
+app.use('/assets', express.static(__dirname + '/assets'))
+app.use('/images', express.static(__dirname + '/images'))
 
 app.get('/', (req, res) => {
-  res.send('izsdasd');
-});
+    res.sendFile(__dirname + '/index.html', () => {
+        res.end()
+    })
+})
 
 app.listen(port, () => {
-  console.log(`listening on port ${port}`);
-});
+  console.log(`listening on port ${port}`)
+})
